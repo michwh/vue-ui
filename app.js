b40115e64534c440e21ac18de50428bdde029d32
wenhui-huang <fjsxhwh@163.com>
2018-02-04 22:03:24 +0800

    编写app.js

--- app.js
+++ app.js
@@ -9,3 +9,20 @@ const router = koaRouter();
 
 app.use(koaBodyparser());
 app.use(koaLogger());
+
+app.use(async (ctx, next) =>{
+   let start = new Date();
+   await next();
+   let ms = new Date() - start;
+   console.log('%s %s - %s',ctx.method,ctx.url,ms);
+});
+
+app.on('error',(err,ctx) =>{
+	console.log('server error',err);
+});
+
+app.listen(3001, () => {
+    console.log('Koa is listening in 3001');
+});
+
+export default app;
